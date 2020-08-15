68b87520deed9b18ac94cccf5c110da6f7cb56b4
Fotis Athineos <fotis@transifex.com>
2020-08-16 00:37:58 +0300

    replace vanilla js with vue

---
+++ albums.js
@@ -0,0 +1,62 @@
+var albums = [
+  {
+    id: 1,
+    artist: "Henry Texier",
+    name: "Varech",
+    musicbrainz_release: "1fa0d391-bfc1-48c1-83ad-33df8f1481a7",
+  },
+  {
+    id: 2,
+    artist: "Roberto Musci",
+    name: "Tower of Silence",
+    musicbrainz_release: "82342b9a-2548-463f-99ba-eae23f2c60e8",
+  },
+  {
+    id: 3,
+    artist: "Broadcast",
+    name: "Tender Buttons",
+    musicbrainz_release: "111349e0-5427-4dfb-b419-e2e689861ebf",
+  },
+  {
+    id: 4,
+    artist: "The RH Factor",
+    name: "Hard Groove",
+    musicbrainz_release: "832c274d-6b8d-4292-b3f5-f78dbfed373a",
+  },
+  {
+    id: 5,
+    artist: "Ramsey Lewis",
+    name: "Mother Nature's Son",
+    musicbrainz_release: "1d5461d0-07c0-431a-b5b3-e7f8088642cd",
+  },
+  {
+    id: 6,
+    artist: "Sleaford Mods",
+    name: "Key Markets",
+    musicbrainz_release: "f169af39-ef18-4e88-8cd2-93ee9042a552",
+  },
+  {
+    id: 7,
+    artist: "Muddy Waters",
+    name: "After the Rain",
+    musicbrainz_release: "ece11094-6655-4ff8-aa94-5e9bf0d06468",
+  },
+  {
+    id: 8,
+    artist: "Kurt Vile",
+    name: "Bottle it In",
+    musicbrainz_release: "ece11094-6655-4ff8-aa94-5e9bf0d06468",
+  },
+  {
+    id: 9,
+    artist: "Brian Eno",
+    name: "Another Green World",
+    musicbrainz_release: "ae71c892-ecb6-4834-b520-6c88351e3b6c",
+  },
+  {
+    id: 10,
+    artist: "Courtney Barnett",
+    name: "Tell Me How You Really Feel",
+    musicbrainz_release: "27e3197a-7c02-4a27-b135-9768e1baecc6",
+  },
+];
--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-/*
-  Color Palette: Red #f15c42
- */
-var colors = ["red", "green", "blue"];
-var random_color = colors[Math.floor(Math.random() * colors.length)]
-document.addEventListener('DOMContentLoaded', function () {
-    img = document.querySelector("header [id=logo]")
-    img.src = img.src.replace("blue", random_color)
-
-    var overlays = document.querySelectorAll('div[class*="overlay"]');
-    for (var i = 0; i < overlays.length; i++) {
-        overlays[i].classList.add(random_color);
-    }
-});
+var app = new Vue({
+  el: "#app",
+  data: {
+    // shuffle items order
+    albumList: albums.sort(() => 0.5 - Math.random()),
+    // random color
+    color: ["red", "blue", "green"].sort(() => 0.5 - Math.random())[0],
+  }
+})
