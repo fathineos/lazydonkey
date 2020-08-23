15d93f129140d795319eb34114d02afa962abe2f
Fotis Athineos <fotis@transifex.com>
2020-08-23 20:21:16 +0300

    Mood selector
    - Mobile friendly version
    - Add albums

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
+var moodAll = "all";
+
+function initializeMoods() {
+  distinctMoods = new Set(
+    ["all"].concat(
+      albums.reduce(
+        (acc, cur) => acc.concat(cur.mood),
+        []
+      ).sort()
+    )
+  );
+  moodList = []
+  distinctMoods.forEach(
+    mood => moodList.push({name: mood, isActive: mood === moodAll})
+  );
+  return moodList;
+}
+
+function initializeAlbumList() {
+  return randomizeList(albums);
+}
+
+function randomizeList(list) {
+  return list.sort(
+    () => 0.5 - Math.random()
+  );
+}
+
 var app = new Vue({
   el: "#app",
   data: {
     // shuffle items order
-    albumList: albums.sort(() => 0.5 - Math.random()),
+    albumList: initializeAlbumList(),
+    moodList: initializeMoods(),
     // random color
-    color: ["red", "blue", "green"].sort(() => 0.5 - Math.random())[0],
+    color: randomizeList(["red", "blue", "green"])[0],
+  },
+  methods: {
+    filter_mood: function (event) {
+      moodName = event.currentTarget.id.split("-")[1];
+
+      // update active mood
+      this.moodList.forEach(
+        mood => this.$set(mood, 'isActive',  moodName === mood.name)
+      );
+
+      // update album list
+      if (moodName === moodAll) {
+        newAlbumList = albums;
+      } else {
+        newAlbumList = albums.filter(album => album.mood.indexOf(moodName) >= 0);
+      }
+
+      this.albumList = randomizeList(newAlbumList);
+    }
   }
-})
+});
